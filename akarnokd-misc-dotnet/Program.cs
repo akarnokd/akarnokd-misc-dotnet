f9742d3342e11d2b096808ec8d0149d768cca7e5
Dávid Karnok <akarnokd@gmail.com>
2016-07-03 00:19:04 +0200

    Add project files.

---
+++ akarnokd-misc-dotnet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace akarnokd_misc_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
